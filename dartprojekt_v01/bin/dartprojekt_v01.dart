b24b8d7ccd2af5220a63ef8fb4b5382aadeff704
suesias <49499209+suesias@users.noreply.github.com>
2025-09-26 00:40:51 +0200

    Update dartprojekt_v01.dart
    Lagt till kontroller för input

--- dartprojekt_v01/bin/dartprojekt_v01.dart
+++ dartprojekt_v01/bin/dartprojekt_v01.dart
@@ -1,19 +1,53 @@
 import 'dart:io';
 
 void main() {
-  print('Skriv första talet:');
-  int num1 = int.parse(stdin.readLineSync()!);
-
-  print('Vilken operation + eller -');
-  var oper = stdin.readLineSync();
-
-  print('Skriv andra talet:');
-  int num2 = int.parse(stdin.readLineSync()!);
   
-  if (oper == '-') {
+  int num1;
+  int num2;
+  var operator;
+
+  while (true) {
+    print('Skriv första talet:');
+    String? input = stdin.readLineSync(); //String? betyder att den kan vara null
+
+    try {
+      num1 = int.parse(input!); // Försök att konvertera till int
+      break; // Success avsluta loopen
+    } catch (e) {
+      print('Du måste skriva ett heltal. Försök igen.');
+    }
+  }
+
+  while (true) {
+    print('Vilken operation + eller -');
+    String? input = stdin.readLineSync();
+
+    if (input == '+' || input == '-') {
+      operator = input; 
+      break; // Success avsluta loopen
+    } else {
+      print('Du måste skriva + eller - Försök igen.');
+    }
+  }
+
+
+ while (true) {
+    print('Skriv andra talet:');
+    String? input = stdin.readLineSync();
+
+    try {
+      num2 = int.parse(input!); // Försök att konvertera till int
+      break; // Success avsluta loopen
+    } catch (e) {
+      print('Du måste skriva ett heltal. Försök igen.');
+    }
+  }
+  
+
+  if (operator == '-') {
     var sum = num1 - num2;
     print('Differensen av talen = $sum');
-  }else if (oper == '+') {
+  }else if (operator == '+') {
     var sum = num1 + num2;
     print('Summan av talen = $sum');
   } else {
