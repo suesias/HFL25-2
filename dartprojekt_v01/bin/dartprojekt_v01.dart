4e72bce72d6282db17aaddc17e4b311588a674a5
suesias <49499209+suesias@users.noreply.github.com>
2025-09-26 00:31:57 +0200

    Update dartprojekt_v01.dart
    Operator tillagd

--- dartprojekt_v01/bin/dartprojekt_v01.dart
+++ dartprojekt_v01/bin/dartprojekt_v01.dart
@@ -4,9 +4,19 @@ void main() {
   print('Skriv första talet:');
   int num1 = int.parse(stdin.readLineSync()!);
 
+  print('Vilken operation + eller -');
+  var oper = stdin.readLineSync();
+
   print('Skriv andra talet:');
   int num2 = int.parse(stdin.readLineSync()!);
-
-  var sum = num1 + num2;
-  print('Summan av talen = $sum');
+  
+  if (oper == '-') {
+    var sum = num1 - num2;
+    print('Differensen av talen = $sum');
+  }else if (oper == '+') {
+    var sum = num1 + num2;
+    print('Summan av talen = $sum');
+  } else {
+    print('Ogiltig operation');
+  }
 }
